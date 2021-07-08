430d64b6323c9b539ed52195e7877deebf0f0c3b
Diego <diego.edo.valdes@gmail.com>
2021-07-08 19:21:36 -0400

    new commit

--- herencia.rb
+++ herencia.rb
@@ -27,7 +27,7 @@ class DailyAppointment < Appointment
         @hour == hour && @min == min
     end
     def to_s
-        # "Reunión diaria en #{self.location} sobre #{self.purpose} a la(s) #{self.hour}:#{self.min}"
+        # "Reunión diaria en #{self.location} sobre #{self.purpose} a la(s) #{self.hour}:#{self.min}" #Se recomienda utilizar esta opción
         "Reunión diaria en #{ @location } sobre #{ @purpose } a la(s) #{ @hour }:#{ @min }"# Tambien funciona
     end
 end
